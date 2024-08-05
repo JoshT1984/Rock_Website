bf2284e97c87b72d958bd3cb7eda48003774478a
xxJrTxx <warlord84502@yahoo.com>
2024-08-04 23:20:28 -0500

    navbar_update

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,14 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import "../css/Navbar.css"
+import "../css/Navbar.css";
 
 function Navbar() {
-
   return (
-    <div id ="navbar">
-      
-    </div>
-  )
+    <>
+      <div id="navbar">
+        <div class = "buttons">
+        <button class = "button">Band Members</button>
+        <button class = "button">Music</button>
+        <button class = "button">Lyrics</button>
+        <button class = "button">Cover Art</button>
+        <button class = "button">Future Projects</button>
+        </div>
+      </div>
+      <div class="bg-container">
+        <div id="bg-image"></div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
