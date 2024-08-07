db963a0e416f3b22334ec4a29404b9506e5e7c97
xxJrTxx <warlord84502@yahoo.com>
2024-08-06 23:40:29 -0500

    added mainpage features

--- src/Main_Page/Main_Page.jsx
+++ src/Main_Page/Main_Page.jsx
@@ -6,7 +6,23 @@ function Main_Page() {
   return (
     <>
       <div class="bg-container">
-        <div id = "main-section"></div>
+        <div id="main-section">
+          <div class = "title-container">
+            <h1>To Bury A Hawk</h1>
+          </div>
+          <div>
+            <ul>
+              <li class = "details"> Date formed: July 07, 2024</li>
+              <li class = "details"> Formed by: Hannah Thompson</li>
+              <li class = "details"> Genre: Classical Rock</li>
+              <li class = "details"> Inspired by: Panic of the Disco</li>
+              <li class = "details">
+                {" "}
+                Instruments in band: Cello, Piano, Electric Guitar, Drums
+              </li>
+            </ul>
+          </div>
+        </div>
         <div id="bg-image"></div>
       </div>
     </>
